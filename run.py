0028732e2471675d05241c8854c5255eb5771804
nidhianilmenon <107086520+nidhianilmenon@users.noreply.github.com>
2022-10-14 21:35:35 -0400

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def time():
+    return str(datetime.now())
 
 app.run(host='0.0.0.0',
         port=8080,
